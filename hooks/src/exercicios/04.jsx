75480db6571c2038248dabe1c9e6a30cfae1758a
LEILANE CASSIMIRO ALVES <1090482321006@fatec-srv.local>
2025-04-11 12:31:55 -0300

    (11/04)-Término do exercicio 04 e Exercicio 05

--- hooks/src/exercicios/04.jsx
+++ hooks/src/exercicios/04.jsx
@@ -3,16 +3,30 @@ import * as React from 'react'
 function Board() {
   // 🐨 squares é o estado para este componente. Adicione useState para squares
   // const squares = Array(9).fill(null)
-   const [squares, setSquares] = React.useState(Array(9).fill(null))
-     // 🐨 Precisaremos dos seguintes itens de estados derivados:
+
+  // Ao inicializar o valor da variável 'squares', verificamos se existe
+  // um estado salvo no localStorage. Caso haja, precisamos conveter o
+  // valor encontrado (que é uma string) para vetor, usando JSON.parse().
+  // Caso não exista valor salvo no localStorage, usamos o valor padrão
+  // Array(9).fill(null) (tabuleiro vazio).
+  // Para garantir que o carregamento a partir do localStorage aconteça
+  // apenas uma vez, por ocasião do carregamento do componente, o valor
+  // inicial da variável de estado será fornecido por uma função () =>
+  // (lazy initializer)
+  const [squares, setSquares] = React.useState(
+    () => JSON.parse(window.localStorage.getItem('squares')) ?? Array(9).fill(null)
+  )
+
+  // 🐨 Precisaremos dos seguintes itens de estados derivados:
   // - nextValue ('X' ou 'O')
   // - winner ('X', 'O', ou null)
   // - status (`Vencedor: ${winner}`, `Deu velha!`, or `Próximo jogador: ${nextValue}`)
   // 💰 Os respectivos cálculos já estão prontos. Basta usar os utilitários 
   // mais abaixo no código para criar essas variáveis
-   const nextValue = calculateNextValue(squares)
-   const winner = calculateWinner(squares)
-   const status = calculateStatus(winner, squares, nextValue)
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
+
   // Esta é a função que o manipulador de clique no quadrado irá chamar. `square`
   // deve ser um índice. Portanto, se você clicar sobre o quadrado central, o
   // valor será `4`.
@@ -29,13 +43,14 @@ function Board() {
     //
     // 🐨 faça uma cópia da matriz dos quadrados
     // 💰 `[...squares]` é do que você precisa!)
-    const SquaresCopy = [...squares]
+    const squaresCopy = [...squares]
     
     // 🐨 ajuste o valor do quadrado que foi selecionado
     // 💰 `squaresCopy[square] = nextValue`
-    SquaresCopy[square] = nextValue
+    squaresCopy[square] = nextValue
+    
     // 🐨 atribua a cópia à matriz dos quadrados
-    setSquares(SquaresCopy)
+    setSquares(squaresCopy)
   }
 
   function restart() {
@@ -51,6 +66,16 @@ function Board() {
       </button>
     )
   }
+
+  // useEffect para salvar o valor da variável de estado squares
+  // no localStorage sempre que ela for atualizada
+  React.useEffect(() => {
+    // localStorage só suporta o armazenamento de valores do tipo
+    // string. Por isso, para guardar o valor da variável de estado
+    // squares, que é um vetor, precisamos antes convertê-lo em 
+    // string usando JSON.stringify().
+    window.localStorage.setItem('squares', JSON.stringify(squares))
+  }, [squares]) // <~ o useEffect somente será executado quando squares for alterado
 
   return (
     <div>
@@ -76,8 +101,8 @@ function Board() {
       </button>
       <hr />
       {
-        squares.map((val, idx) =>(
-          <p>{idx} =&gt; '{val}'  </p>
+        squares.map((val, idx) => (
+          <p>{idx} =&gt; '{val}'</p>
         ))
       }
     </div>
