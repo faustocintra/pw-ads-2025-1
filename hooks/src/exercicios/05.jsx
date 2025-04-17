a3c75b02abcc34eff14bd82067637f00e586d34c
LEONARDO TAVARES PAVAN <1090482321001@fatec-srv.local>
2025-04-17 12:31:45 -0300

    (17/04) inicio dp exercicio 06

--- hooks/src/exercicios/05.jsx
+++ hooks/src/exercicios/05.jsx
@@ -3,6 +3,7 @@ import VanillaTilt from 'vanilla-tilt'
 
 function Tilt({children}) {
   // 🐨 crie uma ref aqui usando React.useRef()
+  const tiltRef = React.useRef()
 
   // 🐨 adicione uma função `React.useEffect` aqui e use VanillaTilt para
   // fazer sua div parecer fantástica.
@@ -16,25 +17,27 @@ function Tilt({children}) {
   // })
 
   React.useEffect(() => {
-    const tiltNode = document.getElementById('tilt-root')
+    // const tiltNode = document.getElementById('tilt-root')
+    alert('Componente carregado!')
+    const tiltNode = tiltRef.current
     VanillaTilt.init(tiltNode, {
       max: 25,
       speed: 400,
       glare: true,
       'max-glare': 0.5
     })
-  }, [])
-  
-  // 💰 Não se esqueça de retornar uma função de limpeza. VanillaTilt.init 
-  // vai adicionar um objeto ao seu DOM, precisando ser eliminado:
-  // `return () => tiltNode.vanillaTilt.destroy()`
-  
-  // 💰 Não se esqueça de especificar seu vetor de dependências! No nosso
+    // 💰 Não se esqueça de retornar uma função de limpeza. VanillaTilt.init 
+    // vai adicionar um objeto ao seu DOM, precisando ser eliminado:
+    return () => {
+      alert('Componente descarregado!')
+      tiltNode.vanillaTilt.destroy()
+    }
+  }, []) // 💰 Não se esqueça de especificar seu vetor de dependências! No nosso
   // caso, samemos que o nodo do tilt nunca muda, então ajuste o vetor para `[]`.
 
   // 🐨 adicione a prop `ref` à div `tilt-root` aqui:
   return (
-    <div className="tilt-root" id="tilt-root">
+    <div className="tilt-root" ref={tiltRef}>
       <div className="tilt-child">{children}</div>
     </div>
   )
