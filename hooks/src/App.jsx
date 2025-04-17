93a8751defc11b3ed3bd44a98a8b794338cb778f
MARCOS VINICIUS DE CASTRO PAVANE <1090482321017@fatec-srv.local>
2025-04-17 12:29:02 -0300

    (17/04) Início do exercício 06

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -6,6 +6,7 @@ import Exercicio02 from './exercicios/02'
 import Exercicio03 from './exercicios/03'
 import Exercicio04 from './exercicios/04'
 import Exercicio05 from './exercicios/05'
+import Exercicio06 from './exercicios/06'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <li> <Link to="/03">Exercício 03</Link></li>
         <li> <Link to="/04">Exercício 04</Link></li>
         <li> <Link to="/05">Exercício 05</Link></li>
+        <li> <Link to="/06">Exercício 06</Link></li>
       </ul>
 
       <hr />
@@ -34,6 +36,7 @@ function App() {
         <Route path="/03" element={ <Exercicio03 /> } />
         <Route path="/04" element={ <Exercicio04 /> } />
         <Route path="/05" element={ <Exercicio05 /> } />
+        <Route path="/06" element={ <Exercicio06 /> } />
       </Routes>
 
     </BrowserRouter>
