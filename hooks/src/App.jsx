6f2ccb93e6ff02611fc3906976ba1594dc32c9e5
Fausto Cintra <professor@faustocintra.com.br>
2025-05-08 12:28:30 -0300

    (08/05) Criação do projeto Karangos

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -7,6 +7,7 @@ import Exercicio03 from './exercicios/03'
 import Exercicio04 from './exercicios/04'
 import Exercicio05 from './exercicios/05'
 import Exercicio06 from './exercicios/06'
+import Prova1 from './exercicios/Prova1'
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <li> <Link to="/04">Exercício 04</Link> </li>
           <li> <Link to="/05">Exercício 05</Link> </li>
           <li> <Link to="/06">Exercício 06</Link> </li>
+          <li> <Link to="/prova1">Prova 1</Link> </li>
         </ul>
         
         <hr />
@@ -36,7 +38,7 @@ function App() {
           <Route path="/03" element={ <Exercicio03 /> } />
           <Route path="/04" element={ <Exercicio04 /> } />
           <Route path="/05" element={ <Exercicio05 /> } />
-          <Route path="/06" element={ <Exercicio06 /> } />
+          <Route path="/prova1" element={ <Prova1 /> } />
         </Routes>
       </BrowserRouter>
     </>
