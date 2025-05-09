c541e25518d4d7008af3bd8562b5adf75619a038
Fausto Cintra <professor@faustocintra.com.br>
2025-05-09 09:15:28 -0300

    (09/05) Término da TopBar e início da FooterBar

---
+++ karangos/src/ui/FooterBar.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography'
+import LocalCafeIcon from '@mui/icons-material/LocalCafe';
+
+export default function FooterBar() {
+  return (
+    <Box 
+      component="footer" 
+      sx={{ 
+        backgroundColor: 'action.disabledBackground',
+        display: 'flex',
+        justifyContent: 'center',
+        position: 'fixed',  // posição fixa
+        bottom: 0,          // na parte de baixo da página
+        width: '100vw'      // 100% da largura da viewport
+      }}
+    >
+      <Typography variant="caption" gutterBottom>
+        Desenvolvido e mantido com <LocalCafeIcon fontSize="small" /> por <a href="mailto:professor@faustocintra.com.br">Prof. Fausto Cintra</a>
+      </Typography>
+    </Box>
+  );
+}
