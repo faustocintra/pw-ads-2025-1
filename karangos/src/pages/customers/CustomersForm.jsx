00925ee1437a8fb5001964a9c921511ecc338c70
LEONARDO TAVARES PAVAN <1090482321001@fatec-srv.local>
2025-06-13 07:59:51 -0300

    (05/03) Término do código do formulário de clientes (COM ERROS)

--- karangos/src/pages/customers/CustomersForm.jsx
+++ karangos/src/pages/customers/CustomersForm.jsx
@@ -4,17 +4,17 @@ import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import MenuItem from '@mui/material/MenuItem'
 import Button from '@mui/material/Button'
-import InputMask from 'react-input-mask'
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { ptBR } from 'date-fns/locale/pt-BR'
 import { parseISO } from 'date-fns'
 import { feedbackWait, feedbackNotify, feedbackConfirm } from '../../ui/Feedback'
 import { useNavigate, useParams } from 'react-router-dom'
+import { useMask } from '@react-input/mask'
 
 export default function CustomersForm() {
 
-  const states = [
+  const brazilianStates = [
     { value: 'DF', label: 'Distrito Federal' },
     { value: 'ES', label: 'Espírito Santo' },
     { value: 'GO', label: 'Goiás' },
@@ -25,10 +25,22 @@ export default function CustomersForm() {
     { value: 'SP', label: 'São Paulo' }
   ]
 
-  const phoneMaskFormatChars = {
-    '9': '[0-9]',     // Somente dígitos
-    '%': '[\s0-9]'    // Dígitos ou espaço em branco (\s)
-  }
+  const identDocumentRef = useMask({
+    mask: "###.###.###-##",
+    replacement: { 
+      '#': /[0-9]/,   // Somente dígitos
+    },
+    showMask: false
+  })
+
+  const phoneRef = useMask({
+    mask: "(##) %####-####",
+    replacement: { 
+      '#': /[0-9]/,   // Somente dígitos
+      '%': /[0-9\s]/  // Dígitos ou espaço em branco (\s)
+    },
+    showMask: false
+  })
 
   // Por padrão, todos os campos começam com uma string vazia como valor.
   // A exceção é o campo birth_date, do tipo data, que, por causa do
@@ -74,7 +86,7 @@ export default function CustomersForm() {
       const response = await fetch(
         import.meta.env.VITE_API_BASE + `/customers/${params.id}`
       )
-      const result = response.json()
+      const result = await response.json()
 
       // Converte o formato de data armazenado no banco de dados
       // para o formato reconhecido pelo componente DatePicker
@@ -129,15 +141,214 @@ export default function CustomersForm() {
           { ...reqOptions, method: 'PUT' }
         )
       }
+      // Senão, envia com o método POST para criar um novo registro
+      else {
+        await fetch(
+          import.meta.env.VITE_API_BASE + `/customers`,
+          { ...reqOptions, method: 'POST' }
+        )
+      }
+
+      feedbackNotify('Item salvo com sucesso.', 'success', 2500, () => {
+        // Retorna para a página de listagem
+        navigate('..', { relative: 'path', replace: true })
+      })
     }
     catch(error) {
-
+      console.error(error)
+      feedbackNotify('ERRO: ' + error.message, 'error')
     }
+    finally {
+      feedbackWait(false)
+    }
+  }
+
+  async function handleBackButtonClick() {
+    if(
+      formModified &&
+      ! await feedbackConfirm('Há informações não salvas. Deseja realmente sair?')
+    ) return    // Sai da função sem fazer nada
+
+    // Aqui o usuário respondeu que quer voltar e perder os dados
+    navigate('..', { relative: 'path', replace: 'true' })
   }
 
   return <>
     <Typography variant="h1" gutterBottom>
       Cadastro de clientes
     </Typography>
+
+    <Box className="form-fields">
+      <form onSubmit={handleFormSubmit}>
+
+        {/* autoFocus ~> foco do teclado no primeiro campo */}
+        <TextField 
+          variant="outlined"
+          name="name"
+          label="Nome completo"
+          fullWidth
+          required
+          autoFocus
+          value={customer.name}
+          onChange={handleFieldChange}
+        />
+
+        <TextField
+          inputRef={identDocumentRef}
+          variant="outlined"
+          name="ident_document"
+          label="CPF"
+          fullWidth
+          required
+          value={customer.ident_document}
+          onChange={handleFieldChange}
+        />
+
+        {/* 
+          O evento onChange do componente DatePicker não passa o parâmetro
+          "event", como o TextField, e sim a própria data que foi modificada.
+          Por isso, ao chamar a função handleFieldChange() no DatePicker,
+          precisamos criar um parâmetro "event" "fake" com as informações
+          necessárias.
+        */}
+        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
+          <DatePicker 
+            label="Data de nascimento"
+            value={customer.birth_date}
+            slotProps={{
+              textField: {
+                variant: "outlined",
+                fullWidth: true
+              }
+            }}
+            onChange={ date => {
+              const event = { target: { name: 'birth_date', value: date } }
+              handleFieldChange(event)
+            }}
+          />
+        </LocalizationProvider>
+
+        <TextField 
+          variant="outlined"
+          name="street_name"
+          label="Logradouro"
+          placeholder="Rua, Av., etc."
+          fullWidth
+          required
+          value={customer.street_name}
+          onChange={handleFieldChange}
+        />
+
+        <TextField 
+          variant="outlined"
+          name="house_number"
+          label="nº"
+          fullWidth
+          required
+          value={customer.house_number}
+          onChange={handleFieldChange}
+        />
+
+        <TextField
+          variant="outlined"
+          name="complements"
+          label="Complemento"
+          placeholder="Casa, apto., bloco, etc."
+          fullWidth
+          value={customer.complements}
+          onChange={handleFieldChange}
+        />
+
+        <TextField 
+          variant="outlined"
+          name="district"
+          label="Bairro"
+          fullWidth
+          required
+          value={customer.district}
+          onChange={handleFieldChange}
+        />
+
+        <TextField 
+          variant="outlined"
+          name="municipality"
+          label="Município"
+          fullWidth
+          required
+          value={customer.municipality}
+          onChange={handleFieldChange}
+        />
+
+        <TextField
+          variant="outlined" 
+          name="state"
+          label="UF" 
+          fullWidth
+          required
+          value={customer.state}
+          select
+          onChange={handleFieldChange}
+        >
+          {
+            brazilianStates.map(s => 
+              <MenuItem key={s.value} value={s.value}>
+                {s.label}
+              </MenuItem>
+            )
+          }
+        </TextField>
+
+        <TextField
+          inputRef={phoneRef}
+          variant="outlined"
+          name="phone"
+          label="Telefone/celular"
+          fullWidth
+          required
+          value={customer.phone}
+          onChange={handleFieldChange}
+        />
+
+        <TextField 
+          variant="outlined"
+          name="email"
+          label="E-mail"
+          fullWidth
+          required
+          value={customer.email}
+          onChange={handleFieldChange}
+        />
+
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          width: '100%'
+        }}>
+          <Button
+            variant="contained"
+            color="secondary"
+            type="submit"
+          >
+            Salvar
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={handleBackButtonClick}
+          >
+            Voltar
+          </Button>
+        </Box>
+
+        <Box sx={{
+          fontFamily: 'monospace',
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100vw'
+        }}>
+          { JSON.stringify(customer, null, ' ') }
+        </Box>
+
+      </form>
+    </Box>
   </>
 }
